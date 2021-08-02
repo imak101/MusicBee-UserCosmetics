7eef7e2c34bbd4dca4eca78823e6b856f0b0243a
imak101 <mftrenkler@gmail.com>
2021-08-01 23:03:12 -0600

    add base classes for updater feature

---
+++ Updater/ApiRequests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using MusicBeePlugin.Form.Popup;
+
+
+namespace MusicBeePlugin.Updater
+{
+    public abstract class ApiRequests
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly Uri _uriReleases = new Uri("https://api.github.com/repos/imak101/MusicBeeSomething1/releases");
+
+        protected ApiRequests() {}
+        
+        private async Task<HttpResponseMessage> ReleasesResponseRaw()
+        {
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _uriReleases))
+            {
+                request.Headers.UserAgent.Add(ProductInfoHeaderValue.Parse("request"));
+
+                return await _client.SendAsync(request);
+            }
+        }
+
+        private async Task<string> ReleasesResponseString()
+        {
+            HttpResponseMessage response = await ReleasesResponseRaw();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        protected async Task<string> ParseResponseStringForValueFromKey(string key)
+        {
+            string response = await ReleasesResponseString();
+
+            try
+            {
+                string match = Regex.Match(response, $"\"{key}[^,]*").Value;
+
+                match = match.Replace("\"", " ").Trim();
+                string[] splitS = match.Split(new []{':'}, 2);
+
+                return splitS[1].Trim();
+            }
+            catch (Exception e)
+            {
+                return $"An exception ({e.Message}) occured while parsing ResponseString from a key.";
+            }
+        }
+        
+        public async void sss()
+        {
+            Debug.WriteLine(await ReleasesResponseString());
+            Debug.WriteLine(await ParseResponseStringForValueFromKey("html_url"));
+            Debug.WriteLine(await ParseResponseStringForValueFromKey("published_at"));
+            Debug.WriteLine(await ParseResponseStringForValueFromKey("tag_name"));
+            //GitHubRelease ghRelease = await MakeGitHubRelease();
+            //new Form_Popup($"{ghRelease.HtmlUrl}...\n..{ghRelease.TagName}", "TITLE");
+        }
+    }
+}
---
+++ Updater/GitHubRelease.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MusicBeePlugin.Updater
+{
+    public class GitHubRelease : ApiRequests
+    {
+        public string TagName { get; }
+        public string HtmlUrl { get; }
+        
+        /// <summary>
+        /// Must call MakeGitHubReleaseAsync() to populate class data
+        /// </summary>
+        public GitHubRelease(){}
+
+        private GitHubRelease(string tagName, string htmlUrl)
+        {
+            TagName = tagName;
+            HtmlUrl = htmlUrl;
+            //TODO: implement datetime
+        }
+        
+        public async Task<GitHubRelease> MakeGitHubReleaseAsync()
+        {
+            return new GitHubRelease(await ParseResponseStringForValueFromKey("tag_name"), await ParseResponseStringForValueFromKey("html_url"));
+        }
+    }
+}
